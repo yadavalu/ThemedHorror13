8fef4a7894fcb793a6070b8c89b541e6b56aabeb
Aadith Yadav <aadith.ygalu@gmail.com>
2023-06-08 17:46:17 +0200

    Added spritesheet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+
 from background import get_background
+from spritesheet import SpriteSheet
 
 pygame.init()
 (w, h) = (1000, 900)
@@ -10,12 +12,39 @@ running = True
 
 background, bg_image = get_background()
 
+ghost_data = {
+    "walk_forward": {
+        "n": 6,
+        "index": 0
+    },
+    "walk_left": {
+        "n": 6,
+        "index": 1
+    },
+    "walk_right": {
+        "n": 6,
+        "index": 2
+    },
+    "walk_away": {
+        "n": 6,
+        "index": 3
+    },
+    "dimensions": [48, 48]
+}
+
+ghost = SpriteSheet(screen, 100, 100, pygame.image.load("ghost.png"), ghost_data)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill((0, 0, 0))
+
     for tile in background:
         screen.blit(bg_image, tile)
+
+    ghost.animate("walk_forward")
+    ghost.render()
+
     pygame.display.update()
---
+++ spritesheet.py
@@ -0,0 +1,42 @@
+import pygame
+
+
+class SpriteSheet:
+    def __init__(self, screen, x, y, img, data):
+        self.screen = screen
+
+        self.x = x
+        self.y = y
+        self.img = img
+        self.render_img = pygame.Surface((0, 0))
+
+        self.data = data
+        self.index = 0
+
+        self.curr_action = None
+
+    def get_size(self, x, y, width, height):
+        self.render_img = pygame.Surface((width, height), pygame.SRCALPHA).convert_alpha()
+        self.render_img.blit(self.img, (0, 0), (x, y, width, height))
+
+    def render(self):
+        self.screen.blit(self.render_img, (self.x, self.y))
+
+    def handle_event(self):
+        pass
+
+    def animate(self, action):
+        if self.curr_action is None:
+            self.index = 0
+            self.curr_action = action
+        else:
+            if action == self.curr_action:
+                self.index += 1
+            else:
+                self.index = 0
+                self.curr_action = action
+
+        if self.index == self.data[action]["n"] - 1:
+            self.index = 0
+
+        self.get_size(self.index, self.data[action]["index"], *self.data["dimensions"])
