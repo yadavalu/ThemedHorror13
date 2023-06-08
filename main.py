e5ffda40beb0ceff686c50bc3a541e8b2dac79f0
yamunan-bitset <yamunan.bitset@gmail.com>
2023-06-08 16:39:47 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+(w, h) = (1000, 900)
+screen = pygame.display.set_mode((w, h))
+pygame.display.set_caption("Horror")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.update()
