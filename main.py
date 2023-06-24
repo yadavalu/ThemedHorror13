fecf3c28b1ac6ac6fecad07447f9032da643a11d
ARCSCH(X) <yamunan.bitset@gmail.com>
2023-06-24 18:10:31 +0200

    added replay button

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from widgets import Button
 import play
+import importlib
 
 pygame.init()
 (w, h) = (1000, 900)
@@ -12,12 +13,16 @@ font = pygame.font.SysFont(None, 30)
 b = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Play", (183, 183, 183), 50, 200, 600, 50)
 o = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Options", (183, 183, 183), 50, 300, 600, 50)
 q = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Quit", (183, 183, 183), 50, 400, 600, 50)
+r = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Replay", (183, 183, 183), 50, 500, 600, 50)
 reimport = False
 
 while True:
     dt = clock.tick(60)
     pos = pygame.mouse.get_pos()
     for event in pygame.event.get():
+        if r.handle_event(event, pos):
+            importlib.reload(play)
+            play.play()
         if b.handle_event(event, pos):
             play.play()
         if q.handle_event(event, pos):
@@ -30,6 +35,7 @@ while True:
             exit(0)
     screen.fill((36, 34, 30))
     b.render()
+    r.render()
     o.render()
     q.render()
     pygame.display.update()
