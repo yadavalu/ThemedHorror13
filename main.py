b8f5376cbe3264b3c15575f2dcf8f67216cbe560
Aadith Yadav <aadith.ygalu@gmail.com>
2023-06-25 15:06:36 +0200

    Fixed replay

--- main.py
+++ main.py
@@ -20,19 +20,18 @@ b = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Play", (18
 i = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Instructions", (183, 183, 183), 200, 400, 600, 50)
 o = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Options", (183, 183, 183), 200, 500, 600, 50)
 q = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Quit", (183, 183, 183), 200, 600, 600, 50)
-r = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Replay", (183, 183, 183), 200, 700, 600, 50)
-
+r_play = False
 sfx.bgm()
 
 while True:
     dt = clock.tick(60)
     pos = pygame.mouse.get_pos()
     for event in pygame.event.get():
-        if r.handle_event(event, pos):
-            importlib.reload(play)
-            play.play()
         if b.handle_event(event, pos):
+            if r_play:
+                importlib.reload(play)
             play.play()
+            r_play = True
         if q.handle_event(event, pos):
             pygame.quit()
             exit(0)
@@ -43,11 +42,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
+
+    if r_play:
+        b.text = "Replay"
+
     screen.fill((36, 34, 30))
     l.render()
     l2.render()
     b.render()
-    r.render()
     i.render()
     o.render()
     q.render()
