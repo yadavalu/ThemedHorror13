170e4f745cc5d7773d767d2f7567c14d23364f1a
ARCSCH(X) <yamunan.bitset@gmail.com>
2023-06-28 17:41:42 +0200

    implemented secondary theme unlucky number (13)

--- instructions.py
+++ instructions.py
@@ -10,7 +10,7 @@ def instructions():
     main_menu_button = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Menu", (183, 183, 183), 5, 5, 100, 50)
     inst = [
         Label(screen, pygame.font.SysFont(None, 100), "Instructions", (200, 100, 100), 450, 200, 100, 50),
-        Label(screen, font, "Escape from the ghosts and collect coins to go to the next level", (183, 183, 183), 450, 300, 100, 50),
+        Label(screen, font, "Escape from the ghosts and collect coins to go to the next level (of which there are 13)", (183, 183, 183), 450, 300, 100, 50),
         Label(screen, font, "right arrow key to move right", (183, 183, 183), 450, 400, 100, 50),
         Label(screen, font, "left arrow key to move left", (183, 183, 183), 450, 450, 100, 50),
         Label(screen, font, "up arrow key to move up", (183, 183, 183), 450, 500, 100, 50),
