8b3d064afded7c524e7899aaccb0cdfd7d4f26be
ARCSCH(X) <yamunan.bitset@gmail.com>
2023-06-28 17:33:14 +0200

    fixed menu

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ clock = pygame.Clock()
 font = pygame.font.SysFont(None, 30)
 l = Label(screen,  pygame.font.SysFont(None, 100), "PacGhost", (200, 100, 100), 450, 150, 100, 50)
 l2 = Label(screen,  font, "ThemedHorror13 Game Jam", (183, 183, 183), 450, 225, 100, 50)
-b = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Play", (183, 183, 183), 200, 400, 600, 50)
-i = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Instructions", (183, 183, 183), 200, 500, 600, 50)
-o = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Options", (183, 183, 183), 200, 600, 600, 50)
-q = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Quit", (183, 183, 183), 200, 700, 600, 50)
-r_play = False
+b = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Play", (183, 183, 183), 200, 300, 600, 50)
+i = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Instructions", (183, 183, 183), 200, 400, 600, 50)
+o = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Options", (183, 183, 183), 200, 500, 600, 50)
+q = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Quit", (183, 183, 183), 200, 600, 600, 50)
+r = Button(screen, (180, 20, 10), (180, 80, 10), (10, 75, 20), font, "Replay", (183, 183, 183), 200, 700, 600, 50)
 sfx.bgm()
 
 while True:
@@ -51,8 +51,6 @@ while True:
             pygame.quit()
             exit(0)
 
-    if r_play:
-        b.text = "Replay"
 
     screen.fill((36, 34, 30))
     l.render()
@@ -61,4 +59,5 @@ while True:
     i.render()
     o.render()
     q.render()
+    r.render()
     pygame.display.update()
